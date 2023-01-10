f8a9559619ff21345a98cf42af0a301993bb3599
ashutoshlamba <55332461+ashutoshlamba@users.noreply.github.com>
2023-01-09 18:21:12 -0700

    added get request to get script

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
 import requests
 print(requests.__version__)
 print(requests.get("http://google.com/"))
+script = requests.get("https://raw.githubusercontent.com/ashutoshlamba/404lab1/main/script.py")
+print(script.text)
